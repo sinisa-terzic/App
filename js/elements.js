2b2edc26402197274d0b0263378a1f55f7acb199
sinisa-terzic <sinisat.ita21@gmail.com>
2023-12-21 12:07:11 +0100

    upd

--- js/elements.js
+++ js/elements.js
@@ -1,26 +1,3 @@
-/* function setSectionHeights() {
-    const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
-
-    // Visina headera
-    const headerHeight = document.getElementById('header').offsetHeight;
-
-    // Visina karusela
-    const carouselHeight = document.getElementById('carousel').offsetHeight;
-
-    // Izračun preostale visine za info sekciju
-    const infoHeight = viewportHeight - headerHeight - carouselHeight;
-
-    // Postavite visinu info sekcije
-    document.getElementById('info').style.height = infoHeight + 'px';
-}
-
-// Pozovi funkciju kada se stranica učita
-window.addEventListener('load', setSectionHeights);
-
-// Dodaj događaj za ponovno postavljanje dimenzija kada se prozor promeni
-window.addEventListener('resize', setSectionHeights); */
-
-
 function setSectionHeights() {
     const viewportHeight = window.innerHeight || document.documentElement.clientHeight;
     const margin = 10;
@@ -37,10 +14,10 @@ function setSectionHeights() {
     // Visina data sekcije
     const dataHeight = viewportHeight - headerHeight - carouselHeight - 2 * margin;
 
-    // Postavite visinu info sekcije u viewport height
+
+    // Postavite visinu info sekcije
     document.getElementById('info').style.height = infoHeight + 'px';
 
-    // Postavite visinu data sekcije u pikselima
     document.getElementById('data').style.height = dataHeight + 'px';
     document.getElementById('data').style.marginTop = margin + 'px';
     document.getElementById('data').style.marginBottom = margin + 'px';
@@ -51,4 +28,3 @@ window.addEventListener('load', setSectionHeights);
 
 // Dodaj događaj za ponovno postavljanje dimenzija kada se prozor promeni
 window.addEventListener('resize', setSectionHeights);
-
